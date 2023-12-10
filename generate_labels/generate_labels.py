f3ef4b108646f95de4f7a383b9d0920b46af3329
Anoop CA <anoopafter@gmail.com>
2023-12-10 22:25:29 +0530

    Label generate updates

--- generate_labels/generate_labels.py
+++ generate_labels/generate_labels.py
@@ -2,13 +2,18 @@
 import cv2
 import os
 import scipy
+import pickle
 import matplotlib.pyplot as plt
 from itertools import islice
 import numpy as np
+import keras
 from keras.layers import Dense, Activation, Flatten, Conv2D, Lambda, MaxPooling2D, Dropout
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+
+#def preprocess_input(x):
+#    return x / 127.5 - 1.
 
 def keras_model(image_x, image_y):
     model = Sequential()
@@ -48,8 +53,9 @@ LIMIT = None
 DATA_FOLDER = r'D:\ML_Projects\Self_Driving_Car_Village_Roads\generate_labels'
 TRAIN_FILE = os.path.join(DATA_FOLDER, 'steering_wheel.mp4')
 LABEL_FILE = os.path.join(DATA_FOLDER, 'train_labels.txt')
-TEST_FILE = os.path.join(DATA_FOLDER, 'i10_steering_wheel.mp4')
+TEST_FILE = os.path.join(DATA_FOLDER, 'steering_test_resized.mp4')
 OUTPUT_FILE = os.path.join(DATA_FOLDER, 'out_labels.txt')
+LABEL_MODEL = os.path.join(DATA_FOLDER, 'label_model')
 
 def return_data():
     X = []
@@ -61,7 +67,8 @@ def return_data():
         for line in islice(fp, LIMIT):
             path, angle = line.strip().split()
             y.append(float(angle) * scipy.pi / 180)
-            img = plt.imread(frame)
+            #img = plt.imread(frame)
+            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             features.append(cv2.resize((cv2.cvtColor(img, cv2.COLOR_RGB2HSV))[:, :, 1], (100, 100)))
             ret, frame = cap.read()
     features = np.array(features).astype('float32')
@@ -69,25 +76,37 @@ def return_data():
     return features, labels
 
 def main():
-    features, labels = return_data()
-    features, labels = shuffle(features, labels)
-    train_x, test_x, train_y, test_y = train_test_split(features, labels, test_size=0.3)
-    train_x = train_x.reshape(train_x.shape[0], 100, 100, 1)
-    test_x = test_x.reshape(test_x.shape[0], 100, 100, 1)
-    model = keras_model(100, 100)
-    model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=128, batch_size=32)
+    if not os.path.exists(LABEL_MODEL):    
+        features, labels = return_data()
+        features, labels = shuffle(features, labels)
+        train_x, test_x, train_y, test_y = train_test_split(features, labels, test_size=0.3)
+        train_x = train_x.reshape(train_x.shape[0], 100, 100, 1)
+        test_x = test_x.reshape(test_x.shape[0], 100, 100, 1)
+        model = keras_model(100, 100)
+        model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=1, batch_size=32)
+        model.save(LABEL_MODEL)
+    else:
+        model = keras.models.load_model(LABEL_MODEL)
 
     label_pred = []
     cap = cv2.VideoCapture(TEST_FILE)
     ret, frame = cap.read()
     while True:
-        label_pred.append(model.predict(frame))
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = cv2.resize((cv2.cvtColor(img, cv2.COLOR_RGB2HSV))[:, :, 1], (100, 100))
+        img = img.reshape(1, 100, 100, 1)
+        x = model.predict(img).item()
+        label_pred.append(x)
+        cv2.imshow(str(x), frame)
+        cv2.waitKey(1000)
+        
         ret, frame = cap.read()
         if not ret:
             break
     cap.release()
+    cv2.destroyAllWindows()
     with open(OUTPUT_FILE, 'a') as file:
         for item in label_pred:
             file.write(str(item) + '\n')
-    
+        file.flush()
 main()
